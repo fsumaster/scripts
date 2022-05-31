75b0718c4d8203881dcd4bbe18eaaff3e41b1e41
fsumaster <cloud.data-acct@proton.me>
2022-05-31 14:38:48 -0400

    wget runner

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 #fsumaster
 sudo apt install vnc4server x11vnc xfce4 xfce4-goodies nano firefox gedit -y
 git clone https://github.com/novnc/noVNC
+wget https://raw.githubusercontent.com/fsumaster/scripts/main/runner.sh
 cd noVNC
 chmod +x runner.sh
 sh ./runner.sh
