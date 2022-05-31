53dc01a37f551d6f927059de4f4532c50f5fdfcf
fsumaster <cloud.data-acct@proton.me>
2022-05-31 15:23:28 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 #codeanywhere novnc via browser
 #fsumaster
-sudo apt install vnc4server x11vnc xfce4 xfce4-goodies nano firefox gedit -y
+touch dependencies.sh
+echo 'sudo -i apt install vnc4server x11vnc xfce4 xfce4-goodies nano firefox gedit -y' >> dependencies.sh
+chmod +x dependencies.sh
+touch runner.sh
+echo '#!/bin/bash' >> runner.sh
+echo 'cd noVNC' >> runner.sh
+echo 'vncserver -geometry 1920x1080' >> runner.sh
+echo './utils/novnc_proxy --vnc localhost:5901 --listen localhost:6081' >> runner.sh
+echo 'Be respectful' >> runner.sh
+chmod +x runner.sh
 git clone https://github.com/novnc/noVNC
 wget https://raw.githubusercontent.com/fsumaster/scripts/main/runner.sh
-cd noVNC
-chmod +x runner.sh
-sh ./runner.sh
+sleep 1
+sudo ./dependencies.sh
+./runner.sh
 exit
