c6c8084eade3d812fb67b272c6fcc1baae1c797f
fsumaster <cloud.data-acct@proton.me>
2025-08-31 22:13:29 -0400

    v 1.6 update ubuntu_setup.sh
    new script, added more apps that I normally end up downloading

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,34 +1,54 @@
 #!/bin/bash
-echo #############################################################
-echo #          FSUMASTER - [Ubuntu]-setup script - v1.4         #
-echo #             tested on ubuntu 22.04 LTS                    #
-echo #                                                           #
-echo #                                                           #
-echo #############################################################
-echo         #"free your mind and your ass will follow"#
+echo "#############################################################"
+echo "#          FSUMASTER - [Ubuntu]-setup script - v1.8         #"
+echo "#             tested on Ubuntu 22.04 LTS                    #"
+echo "#############################################################"
+echo "\"free your mind and your ass will follow\""
 
-sudo apt-get update
-echo Install packages
-sudo apt-get install git bleachbit gufw p7zip-rar p7zip-full unace unrar zip unzip sharutils rar uudeview mpack arj cabextract file-roller gnome-tweaks curl
+# Root check
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
 
-echo Setup and install brave browser
-sudo apt install curl
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install brave-browser
+echo "Adding repositories & keys..."
 
-echo Setup and install protonvpn
-sudo add-apt-repository 'deb https://repo.protonvpn.com/debian unstable main'
-sudo apt-get update
-sudo apt-get install protonvpn
+# Brave Browser
+curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | tee /etc/apt/sources.list.d/brave-browser-release.list
 
-echo Enable ufw and start logging
-sudo ufw enable
-sudo ufw enable logging
+# VSCodium
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | tee /usr/share/keyrings/vscodium-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg] https://download.vscodium.com/debs vscodium main" | tee /etc/apt/sources.list.d/vscodium.list
 
-echo Display hidden startup applications
-sudo sed -i "s/NoDisplay=true/NoDisplay=false/g" /etc/xdg/autostart/*.desktop
-echo #done#
+# ProtonVPN
+add-apt-repository 'deb https://repo.protonvpn.com/debian unstable main' -y
 
-sudo reboot
+echo "Running apt update once..."
+apt-get update -y
+
+echo "Installing packages..."
+apt-get install -y \
+  git bleachbit gufw p7zip-rar p7zip-full unace unrar zip unzip sharutils rar uudeview mpack arj cabextract file-roller \
+  gnome-tweaks curl gnome-shell-extensions rsync \
+  brave-browser steam-installer libvirt-daemon-system libvirt-clients virt-manager \
+  remmina remmina-plugin-rdp remmina-plugin-vnc \
+  codium telegram-desktop keepassxc protonvpn
+
+# Install MegaSync
+echo "Installing MegaSync..."
+wget https://mega.nz/linux/repo/xUbuntu_22.04/amd64/megasync-xUbuntu_22.04_amd64.deb -O /tmp/megasync.deb
+apt install -y /tmp/megasync.deb
+rm -f /tmp/megasync.deb
+
+# Enable firewall
+echo "Enabling UFW..."
+ufw enable
+ufw logging on
+
+# Reveal hidden startup apps
+echo "Revealing hidden startup applications..."
+sed -i "s/NoDisplay=true/NoDisplay=false/g" /etc/xdg/autostart/*.desktop
+
+echo "Installation complete! Rebooting..."
+reboot
